1b604cfa7727f38abb495b190c2fb363958fb4c5
phpontes <ppontes95@hotmail.com>
2025-01-13 12:53:56 -0300

    Configuring query parameters

--- backend/src/main/java/com/paulopontes/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/paulopontes/dscatalog/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.paulopontes.dscatalog.services;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -95,7 +96,13 @@ public class ProductService {
 	}
 
 	@Transactional(readOnly = true)
-	public Page<ProductProjection> testQuery(Pageable pageable) {
-		return repository.searchProducts(Arrays.asList(), "", pageable);
+	public Page<ProductProjection> findAllPaged(String name, String categoryId, Pageable pageable) {
+		
+		List<Long> categoryIds = Arrays.asList();
+		if (!"0".equals(categoryId)) {
+			categoryIds = Arrays.asList(categoryId.split(",")).stream().map(Long::parseLong).toList();
+		}
+		
+		return repository.searchProducts(categoryIds, name, pageable);
 	}
 }
