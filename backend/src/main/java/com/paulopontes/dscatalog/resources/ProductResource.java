00cac78feb731593364cfb0eec4f24691b24bae1
phpontes <ppontes95@hotmail.com>
2025-01-13 13:12:18 -0300

    Products and categories final query with JOIN FETCH

--- backend/src/main/java/com/paulopontes/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/paulopontes/dscatalog/resources/ProductResource.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.paulopontes.dscatalog.dto.ProductDTO;
-import com.paulopontes.dscatalog.projections.ProductProjection;
 import com.paulopontes.dscatalog.services.ProductService;
 
 import jakarta.validation.Valid;
@@ -31,11 +30,11 @@ public class ProductResource {
 	private ProductService service;
 
 	@GetMapping
-	public ResponseEntity<Page<ProductProjection>> findAll(
+	public ResponseEntity<Page<ProductDTO>> findAll(
 			@RequestParam(value = "name", defaultValue = "") String name,
 			@RequestParam(value = "categoryId", defaultValue = "0") String categoryId,
 			Pageable pageable) {		
-		Page<ProductProjection> list = service.findAllPaged(name, categoryId, pageable);
+		Page<ProductDTO> list = service.findAllPaged(name, categoryId, pageable);
 		return ResponseEntity.ok().body(list);
 	}
 
